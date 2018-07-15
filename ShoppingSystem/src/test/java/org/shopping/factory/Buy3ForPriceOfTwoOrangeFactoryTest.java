14129c27960d1b3a117831b455f6da0924020a38
Conroy White <conroyaw@gmail.com>
2018-07-15 08:23:34 -0700

    Update Buy3ForPriceOfTwoOrangeFactoryTest.java

--- ShoppingSystem/src/test/java/org/shopping/factory/Buy3ForPriceOfTwoOrangeFactoryTest.java
+++ ShoppingSystem/src/test/java/org/shopping/factory/Buy3ForPriceOfTwoOrangeFactoryTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
-class Buy3ForPriceOfTwoOrangeFactoryTest {
+public class Buy3ForPriceOfTwoOrangeFactoryTest {
 
 	 @Test
 	    public void buyThreeForThePriceOfTwo() throws Exception {
